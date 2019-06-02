05feded3c87493fb58a739391fdccc099e1a2b13
Awais <macheema3004@gmail.com>
2019-06-03 00:43:14 +0500

    peek-a-boo updated

--- src/app/lifecycle-hooks/peek-a-boo.component.ts
+++ src/app/lifecycle-hooks/peek-a-boo.component.ts
@@ -10,15 +10,16 @@ import {
   SimpleChanges
 } from '@angular/core';
 import { Component, Input } from '@angular/core';
-import { LoggerService }    from './logger.service';
+import { LoggerService } from './logger.service';
 
 let nextId = 1;
 
 export class PeekABoo implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   constructor(private logger: LoggerService) { }
 
-  // implement OnInit's `ngOnInit` method
-  ngOnInit() { this.logIt(`OnInit`); }
 
   logIt(msg: string) {
     this.logger.log(`#${nextId++} ${msg}`);
@@ -26,6 +27,7 @@ export class PeekABoo implements OnInit {
 }
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'peek-a-boo',
   template: '<p>Now you see my hero, {{name}}</p>',
   styles: ['p {background: LightYellow; padding: 8px}']
@@ -33,51 +35,56 @@ export class PeekABoo implements OnInit {
 // Don't HAVE to mention the Lifecycle Hook interfaces
 // unless we want typing and tool support.
 export class PeekABooComponent extends PeekABoo implements
-             OnChanges, OnInit, DoCheck,
-             AfterContentInit, AfterContentChecked,
-             AfterViewInit, AfterViewChecked,
-             OnDestroy {
-  @Input()  name: string;
+  OnChanges, OnInit, DoCheck,
+  AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked,
+  OnDestroy {
+  @Input() name: string;
 
   private verb = 'initialized';
 
   constructor(logger: LoggerService) {
     super(logger);
 
-    let is = this.name ? 'is' : 'is not';
+    const is = this.name ? 'is' : 'is not';
     this.logIt(`name ${is} known at construction`);
   }
 
+  // implement OnInit's `ngOnInit` method
+  ngOnInit() { this.logIt(`OnInit name ${this.verb} to "${this.name}"`); }
   // only called for/if there is an @input variable set by parent.
   ngOnChanges(changes: SimpleChanges) {
-    let changesMsgs: string[] = [];
-    for (let propName in changes) {
+    const changesMsgs: string[] = [];
+    for (const propName in changes) {
       if (propName === 'name') {
-        let name = changes['name'].currentValue;
+        const name = changes['name'].currentValue;
         changesMsgs.push(`name ${this.verb} to "${name}"`);
+        changesMsgs.push(`name ${this.verb} to "${this.name}"`);
       } else {
         changesMsgs.push(propName + ' ' + this.verb);
       }
     }
     this.logIt(`OnChanges: ${changesMsgs.join('; ')}`);
-    this.verb = 'changed'; // next time it will be a change
   }
 
   // Beware! Called frequently!
   // Called in every change detection cycle anywhere on the page
-  ngDoCheck() { this.logIt(`DoCheck`); }
+  ngDoCheck() { this.logIt(`DoCheck name ${this.verb} to "${this.name}"`); }
 
-  ngAfterContentInit() { this.logIt(`AfterContentInit`);  }
-
-  // Beware! Called frequently!
-  // Called in every change detection cycle anywhere on the page
-  ngAfterContentChecked() { this.logIt(`AfterContentChecked`); }
-
-  ngAfterViewInit() { this.logIt(`AfterViewInit`); }
+  ngAfterContentInit() { this.logIt(`AfterContentInit name ${this.verb} to "${this.name}"`); }
 
   // Beware! Called frequently!
   // Called in every change detection cycle anywhere on the page
-  ngAfterViewChecked() { this.logIt(`AfterViewChecked`); }
+  ngAfterContentChecked() { this.logIt(`AfterContentChecked name ${this.verb} to "${this.name}"`); }
+
+  ngAfterViewInit() { this.logIt(`AfterViewInit name ${this.verb} to "${this.name}"`); }
+
+  // Beware! Called frequently!
+  // Called in every change detection cycle anywhere on the page
+  ngAfterViewChecked() {
+    this.logIt(`AfterViewChecked name ${this.verb} to "${this.name}"`);
+    this.verb = 'changed'; // next time it will be a change
+  }
 
   ngOnDestroy() { this.logIt(`OnDestroy`); }
 }
