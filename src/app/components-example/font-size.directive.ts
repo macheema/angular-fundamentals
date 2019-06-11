b18ad1129cf29675b5f7f84686dc71bb4443af74
Awais <macheema3004@gmail.com>
2019-06-12 03:37:10 +0500

    corrected font size behaviour

--- src/app/components-example/font-size.directive.ts
+++ src/app/components-example/font-size.directive.ts
@@ -1,16 +1,18 @@
-import { Directive, ElementRef, Input, Renderer2, OnChanges, AfterViewInit } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2, OnChanges, AfterViewInit, AfterViewChecked, DoCheck } from '@angular/core';
 
 @Directive({
   // tslint:disable-next-line:directive-selector
   selector: '[fontsize]'
 })
-export class FontSizeDirective implements AfterViewInit {
+export class FontSizeDirective implements DoCheck {
+
 
   @Input() fontsize: number;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
   }
-  ngAfterViewInit(): void {
+
+  ngDoCheck(): void {
     this.renderer.setStyle(this.elRef.nativeElement, 'font-size', `${this.fontsize}px`);
   }
 }
